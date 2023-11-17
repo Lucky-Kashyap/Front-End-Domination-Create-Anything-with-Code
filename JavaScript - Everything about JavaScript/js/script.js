390a11ece002123decd818723d4ac1109b8aa034
Lucky <luckykashyap3430@gmail.com>
2023-11-17 21:58:41 +0530

    started with javascript

---
+++ JavaScript - Everything about JavaScript/js/script.js
@@ -0,0 +1,26 @@
+// js the language
+
+// js - breiden eich
+// managed by ECMA
+// naye features, puraane hatana, kharab features shi krna
+
+// js the DOM - creating
+
+// basics
+// inbult features
+// variables constants
+// compilers & interpreters
+// window
+// types
+// conditionals
+// loops
+// functions
+// arrays
+// objects
+// questions
+
+// prototypes
+// prototypal inheritance
+// asynchronous
+// es6 climax
+// questions
