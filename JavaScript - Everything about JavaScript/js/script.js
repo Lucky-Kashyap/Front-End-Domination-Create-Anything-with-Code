b9976726ac5c86d0da7777bc023b893ef14854bf
Lucky <luckykashyap3430@gmail.com>
2023-11-17 22:14:59 +0530

    grammar of js

--- JavaScript - Everything about JavaScript/js/script.js
+++ JavaScript - Everything about JavaScript/js/script.js
@@ -13,10 +13,20 @@
 
 //  basics = file connect karenge and load karnge script ko, basics of code execution, grammar and understanding of errors
 
-var a = 12;
-var b = 22;
+// var a = 12;
+// var b = 22;
 
-console.log("Basics of JS");
+// console.log("Basics of JS");
+
+// kaha ho bhai chalo ghar
+// ho ghar bhai chalo kaha
+
+// grammar of js
+
+// you can have all the tools in your box & grammar is like ability to use them
+
+// you deal can me with
+// you can deal with me
 
 // inbult features
 // variables constants
