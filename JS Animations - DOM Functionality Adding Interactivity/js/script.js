fed8bfe028638f80ac3a1a29ea1faecfc5de102a
Lucky <luckykashyap3430@gmail.com>
2023-11-18 22:38:53 +0530

    understanding DOM

---
+++ JS Animations - DOM Functionality Adding Interactivity/js/script.js
@@ -0,0 +1,25 @@
+// content
+
+// DOM
+
+// DOM Manipulation
+
+// Accessing Elements:
+//      document.querySelector()
+
+// Modifying Elements:
+//      innerHTMl, textContent
+
+// Manipulating Styles and Classes:
+//    style
+//    classList
+
+// Creating and Deleting Elements:
+// createElement()
+// appendChild()
+// removeChild()
+
+// Event Handling:
+//    addEventListener()
+
+// Event Object
---
+++ JS Animations - DOM Functionality Adding Interactivity/js/understanding DOM.js
@@ -0,0 +1,19 @@
+// DOM - Document Object Model
+
+// Jo bhi hum website par dekhte hai wo sab body tag ke andar bnta hai, ab jo body bnta hai whi hme website par dikhta hai, DOM actually hota hai thoda asaan shabdo mein HTML Tag aur uska poora control JS mein
+
+// kabhi aap chaate ho ki aap ek button ko click krke ek div gaayab karde
+
+// HTML mein koi aisa feature nahi hai jo click ko hanlde kar sake, us js ko hum perform kar skte hai DOM ke saath
+
+// aisa js ka koi bhi code jo webpage mein kuchh bhi change karega wo js ka DOM ka code kahlayega
+
+// DOM = HTML
+
+// DOM Manipulation => body mein kuchh bhi change karna ya manipulate karna actucally DOM Manipulation kehlata hai
+
+// usse ye kardo ye hone par
+
+// selection   change     event
+
+// accessing elements
