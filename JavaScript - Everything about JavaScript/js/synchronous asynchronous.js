6673418595a9a43f58340957d73b82c5c0c29218
Lucky <luckykashyap3430@gmail.com>
2023-11-18 18:24:15 +0530

    synchronous code vs asynchronous code

---
+++ JavaScript - Everything about JavaScript/js/synchronous asynchronous.js
@@ -0,0 +1,5 @@
+// synchronous asynchronous
+// synchronous code mein humesha line by line chlta hai
+// async code line by line nahi chalta, saara async code ek saath shuru kar diya jaata hai and jo pehle complete ho jayein uska answer de diya jaata hai.
+
+// indepth
