e9f4e9c26547b98667f737c5a62f53f179e2edd4
Lucky <luckykashyap3430@gmail.com>
2023-11-18 11:54:18 +0530

    compiler vs interpreter

---
+++ JavaScript - Everything about JavaScript/js/compiler vs interpreter.js
@@ -0,0 +1,11 @@
+// compiler vs interpreter
+
+// js language english wwords use krti hai and computers english nahi samjhte, wo samjhte hai 0 and 1 jisko current flow and current bypass bhi kah sakte ho, and hum chahte hai hum english mein likhke computer se baat krle, to hum ek translator lagaayege jo ki humara code english mein lega and use 0 and 1 mein convert kr dega,
+
+// compiler and interpreter translators hai jo ki english code ko convert kr rhe hai machine code mein js mein interpreter hota hai
+
+// java - compiler
+
+// jit compiler - just in time (google)
+
+// interpreter ki shuraati taakat use krta hai and compiler ki running taakat use krta hai
