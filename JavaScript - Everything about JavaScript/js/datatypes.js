30a26d954d3c9e8f6e05016e5ec54f3902f4267c
Lucky <luckykashyap3430@gmail.com>
2023-11-18 12:28:54 +0530

    datatype in js

---
+++ JavaScript - Everything about JavaScript/js/datatypes.js
@@ -0,0 +1,47 @@
+// datatypes & beyond
+
+// types matlab data ka roop
+
+// lucky - string
+// 12 - integer
+// 12.3 - float
+// 22.0 - float
+// true - boolean
+// a - character
+
+//  types - two types
+
+// primitive vs reference
+
+// 12, 12.4 , lucky, true, a, null, undefined
+
+// references
+// [] {} ()
+
+// var a;
+// var a = 12;
+// var a = 12.1;
+// var a = true;
+// var a = []; // references
+// var a = false;
+// var a = null;
+// var a = undefined;
+// var a = function () {}; // references
+
+// var a = 12;
+
+// var b = 20;
+
+// b += 2;
+
+// console.log(a, b);
+
+// value vs reference
+
+var arr = [1, 2, 3, 4, 5];
+
+// var b = arr;
+var b = [...arr];
+
+b.pop();
+console.log(arr, b);
