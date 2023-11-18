4f8eea2cc2f7188d72273f892a1332a5a14b9068
Lucky <luckykashyap3430@gmail.com>
2023-11-18 16:38:42 +0530

    arrays in js

---
+++ JavaScript - Everything about JavaScript/js/arrays.js
@@ -0,0 +1,34 @@
+// arrays
+
+// what
+
+// array matlab jab bhi aapko ek ya ek se jaada value ek saath rakhni ho
+
+// var user1 = 'lucky';
+// var user2 = 'harshita';
+// var user3 = 'harsh';
+
+// var users = ["harsh", "harshita", "harshika", "akansha", "rishi"];
+
+// indexing 0 1 ....
+
+// console.log(users);
+
+// console.log(users[2]);
+
+// console.log((users = 5));
+// console.log(users[100]);
+
+// console.log(users.length);
+
+// console.log(typeof users);
+
+// how
+
+// var arr = [1,2,3,4,5,function(){},[],'3'];
+
+// why
+
+// a lot of times the data is in the shape of more than member, to contain all data altogether, we can save it in an array
+
+// loop with array
