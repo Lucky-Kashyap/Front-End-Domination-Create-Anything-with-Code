5cb3361c2ae09e05012c012607b8b96bb6b36fce
Lucky <luckykashyap3430@gmail.com>
2023-11-18 15:06:13 +0530

    while loop

--- JavaScript - Everything about JavaScript/js/loops.js
+++ JavaScript - Everything about JavaScript/js/loops.js
@@ -39,3 +39,68 @@
 // for (var i = 20; i > 4; i--) {
 //   console.log(i);
 // }
+
+// while
+
+// start;
+// while(end){
+// change;
+// }
+
+// let i = 10;
+
+// while (i < 20) {
+//   console.log(i);
+//   i++;
+// }
+
+// 1 - 10
+
+// var i = 1;
+
+// while (i < 11) {
+//   console.log(i);
+//   i++;
+// }
+
+// 20 - 30
+
+// var i = 20;
+
+// while (i < 31) {
+//   console.log(i);
+//   i++;
+// }
+
+// 32 - 45
+
+// var i = 32;
+
+// while (i < 46) {
+//   console.log(i);
+//   i++;
+// }
+
+// 45 - 32
+
+// var i = 45;
+
+// while (i > 31) {
+//   console.log(i);
+//   i--;
+// }
+
+// 12 - 1
+
+// var i = 12;
+
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// glory with loops
+
+// try to create basic loop structure
+
+// try to conquer basic loop structure ranging from one num to another
