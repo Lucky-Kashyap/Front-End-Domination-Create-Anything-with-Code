7836dcea8783f870a2dfb7e00d4c7c3621e97bcf
Lucky <luckykashyap3430@gmail.com>
2023-11-19 17:57:26 +0530

    high order functions

--- Advance JavaScript - God level JS/js/script.js
+++ Advance JavaScript - God level JS/js/script.js
@@ -211,15 +211,54 @@
 
 // event listener ko parent par lgao and unko id, class ya fir tag ke basis par differentiate karke different task karao
 
-var parent = document.querySelector("#parent");
+// var parent = document.querySelector("#parent");
 
-parent.addEventListener("click", (e) => {
-  // console.log("works");
-  // console.log(e);
-  // console.log(e.target);
-  if (e.target.id === "play") {
-    console.log("play song");
-  } else if (e.target.id === "pause") {
-    console.log("pause song");
-  }
-});
+// parent.addEventListener("click", (e) => {
+// console.log("works");
+// console.log(e);
+// console.log(e.target);
+//   if (e.target.id === "play") {
+//     console.log("play song");
+//   } else if (e.target.id === "pause") {
+//     console.log("pause song");
+//   }
+// });
+
+// High order functions
+
+// aisa koi fnc jo ki ek fnc ko parameter mein accept karle and/or ek function ko return karde
+
+// function abcd(param) {}
+
+// abcd(12);
+
+// function abcd() {
+//   return function () {};
+// }
+
+// var arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((el) => el);
+
+function operator(callback, a, b) {
+  console.log(callback(a, b));
+}
+
+function add(a, b) {
+  return a + b;
+}
+function div(a, b) {
+  return a / b;
+}
+function mul(a, b) {
+  return a * b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+operator(add, 12, 13);
+operator(mul, 12, 13);
+operator(sub, 12, 13);
+operator(div, 12, 13);
