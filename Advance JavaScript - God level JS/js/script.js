5d9318fa5d98229ae4f6c15afcf9ec2ca19a368d
Lucky <luckykashyap3430@gmail.com>
2023-11-19 17:50:13 +0530

    event delegation

--- Advance JavaScript - God level JS/js/script.js
+++ Advance JavaScript - God level JS/js/script.js
@@ -193,14 +193,33 @@
 
 // res();
 
-function timer() {
-  var a = 12;
+// function timer() {
+//   var a = 12;
 
-  return setTimeout(() => {
-    console.log(a);
-  }, 2000);
-}
+//   return setTimeout(() => {
+//     console.log(a);
+//   }, 2000);
+// }
 
-var res = timer();
+// var res = timer();
 
-console.log(res);
+// console.log(res);
+
+// Event Delegation
+
+// event delegation = jab aap event listener se kai saare different elements ke events ko handle kar sake
+
+// event listener ko parent par lgao and unko id, class ya fir tag ke basis par differentiate karke different task karao
+
+var parent = document.querySelector("#parent");
+
+parent.addEventListener("click", (e) => {
+  // console.log("works");
+  // console.log(e);
+  // console.log(e.target);
+  if (e.target.id === "play") {
+    console.log("play song");
+  } else if (e.target.id === "pause") {
+    console.log("pause song");
+  }
+});
