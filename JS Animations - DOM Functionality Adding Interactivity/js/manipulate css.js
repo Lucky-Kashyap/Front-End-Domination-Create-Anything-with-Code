c178b472d69bfa4e2740c654feaa538e1d0e83ad
Lucky <luckykashyap3430@gmail.com>
2023-11-19 22:16:26 +0530

    move box using js

--- JS Animations - DOM Functionality Adding Interactivity/js/manipulate css.js
+++ JS Animations - DOM Functionality Adding Interactivity/js/manipulate css.js
@@ -35,9 +35,39 @@
 
 // jab bhi aap kisi element par kisi bhi prakaar ka koi action krte ho to whaa par event by default automatically raise hota hai ab agar aapne koi event listener nahi banaya hai toh wo event ignore krdiya jaata hai par agar listener banaya hai us event par to wo event ignore nahi kiya jaayega balki uska listener chalega
 
-var btn = document.querySelector("button");
+// var btn = document.querySelector("button");
 
-btn.addEventListener("click", (e) => {
-  console.log(e);
-  console.log("clicked");
+// btn.addEventListener("click", (e) => {
+// console.log(e);
+// console.log("clicked");
+
+//   btn.textContent = "starting";
+//   btn.style.backgroundColor = "red";
+// });
+
+// btn.addEventListener("mouseover", (e) => {
+//   btn.textContent = "starting";
+//   btn.style.backgroundColor = "red";
+// });
+
+// btn.addEventListener("mouseleave", (e) => {
+//   btn.textContent = "click Me";
+//   btn.style.backgroundColor = "#fff";
+// });
+
+var body = document.querySelector("body");
+var box = document.querySelector(".box");
+
+body.addEventListener("mousemove", (e) => {
+  // console.log(e.clientX);
+  // console.log(e.clientY);
+
+  box.style.width = "20px";
+  box.style.height = "20px";
+  box.style.borderRadius = "50%";
+  box.style.position = "absolute";
+
+  box.style.left = e.clientX;
+  box.style.right = e.clientY;
+  box.style.backgroundColor = "red";
 });
