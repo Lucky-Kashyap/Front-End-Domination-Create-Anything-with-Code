ffcbaa702d6caf808d9fd756675eedf2c8286797
Lucky <luckykashyap3430@gmail.com>
2023-11-19 14:42:47 +0530

    more optimize

--- JS Tasks - Mastering practical development challenges/JS Practice Like Hell Task 4/js/script.js
+++ JS Tasks - Mastering practical development challenges/JS Practice Like Hell Task 4/js/script.js
@@ -4,22 +4,29 @@ var btn = document.querySelector("button");
 var input = document.querySelector("input");
 var unorderedList = document.querySelector("ul");
 
+var list;
+
+var deleteBtn;
+
 btn.addEventListener("click", () => {
-  var list = document.createElement("li");
+  if (input.value.trim() === "") {
+  } else {
+    list = document.createElement("li");
 
-  var deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete Element";
+    deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete Element";
 
-  // list.textContent = input.value;
+    // list.textContent = input.value;
 
-  list.textContent = input.value;
+    list.textContent = input.value;
 
-  unorderedList.append(list, deleteBtn);
-  input.value = "";
+    unorderedList.append(list, deleteBtn);
+    input.value = "";
 
-  deleteBtn.addEventListener("click", () => {
-    // list.removeChild();
-    unorderedList.removeChild(list);
-    unorderedList.removeChild(deleteBtn);
-  });
+    deleteBtn.addEventListener("click", () => {
+      // list.removeChild();
+      unorderedList.removeChild(list);
+      unorderedList.removeChild(deleteBtn);
+    });
+  }
 });
