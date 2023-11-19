57bda83f47bb25b43e83ec7602f85ee91e092089
Lucky <luckykashyap3430@gmail.com>
2023-11-19 22:00:25 +0530

    add event listener

--- JS Animations - DOM Functionality Adding Interactivity/js/manipulate css.js
+++ JS Animations - DOM Functionality Adding Interactivity/js/manipulate css.js
@@ -1,14 +1,43 @@
 // Manipulate CSS
 
-var h1 = document.querySelector("h1");
+// var h1 = document.querySelector("h1");
 
 // h1.style.color = "red";
 
+// var btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+// h1.style.color = "red";
+// h1.style.fontSize = "60px";
+
+// h1.classList.add("makeitred");
+// h1.classList.remove("makeitred");
+// });
+
+// var h2 = document.createElement("h2");
+
+// h2.textContent = "Heading 2 with creating element using js";
+
+// document.body.append(h2);
+
+// document.body.appendChild(h2);
+
+// var img = document.createElement("img");
+
+// img.src =
+//   "https://vz-53036e60-5d9.b-cdn.net/e600d9a7-8240-4135-b021-fff9bdfe7fff/preview.webp";
+
+// document.querySelector("body").appendChild(img);
+
+// document.querySelector('body').removeChild(img);
+
+// event handling
+
+// jab bhi aap kisi element par kisi bhi prakaar ka koi action krte ho to whaa par event by default automatically raise hota hai ab agar aapne koi event listener nahi banaya hai toh wo event ignore krdiya jaata hai par agar listener banaya hai us event par to wo event ignore nahi kiya jaayega balki uska listener chalega
+
 var btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
-  // h1.style.color = "red";
-  // h1.style.fontSize = "60px";
-
-  h1.classList.add("makeitred");
+btn.addEventListener("click", (e) => {
+  console.log(e);
+  console.log("clicked");
 });
