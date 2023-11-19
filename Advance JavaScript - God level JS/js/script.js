77c0083d3c47972cd4b99d9b76c28d4f2242964f
Lucky <luckykashyap3430@gmail.com>
2023-11-19 17:26:49 +0530

    prototypal inheritance

--- Advance JavaScript - God level JS/js/script.js
+++ Advance JavaScript - God level JS/js/script.js
@@ -121,13 +121,53 @@
 
 // abcd.apply(obj, [1, 2, 3, 4]);
 
-function abcd() {
-  console.log(this);
+// function abcd() {
+//   console.log(this);
+// }
+
+// abcd();
+// const obj = { name: "lucky" };
+
+// const res = abcd.bind(obj);
+
+// console.log(res());
+
+// prototypal Inheritance
+
+// hum objects create karte hai and kisi ek parent constructor function k eprototype mein kuchh add kr dete hai and jab bhi add hota hai to wo parent se banne waale sabhi chldren instances ko wo properties jo parent ko prototype mein di gayi thi wo milti hai saugat mein.
+
+// function makeHuman(name, age) {
+//   this.name = name;
+//   this.age = age;
+// this.printMyName = function () {
+//   console.log(this.name);
+// };
+// }
+
+//  aisa koi bhi function jismein aap this ka upyog kar rhe ho aur us function ko call karte waqt aap new ka upyog karein, to new ka matlab waha par ek blank object hojaata hai
+
+// blank object
+
+// ek function jo ki this ka upyog kar raha ho and new ke dwara nayein nayein objects bnakar deta ho ese functions ko constructor function kehte hai
+
+function makeHuman(name, age) {
+  this.name = name;
+  this.age = age;
+  // this.printMyName = function () {
+  //   console.log(this.name);
+  // };
 }
 
-abcd();
-const obj = { name: "lucky" };
+makeHuman.prototype.printMyName = function () {
+  console.log(this.name);
+};
 
-const res = abcd.bind(obj);
+const human1 = new makeHuman("Harsh", 25);
+const human2 = new makeHuman("Harshita", 28);
+const human3 = new makeHuman("Lucky", 23);
 
-console.log(res());
+console.log(human1);
+
+human1.printMyName();
+human2.printMyName();
+human3.printMyName();
