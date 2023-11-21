a75f9e3e2d35622589e6bea4b6ba0747e3cedcc6
Lucky <luckykashyap3430@gmail.com>
2023-11-21 22:32:46 +0530

    character count using js

---
+++ JS Tasks - Mastering practical development challenges/JS Practice Like Hell Task 9/js/script.js
@@ -0,0 +1,12 @@
+// Build a character counter for a text area or input field, which updates
+//  real-time as the user types and enforces a character limit.
+
+var char = document.querySelector(".char");
+var word = document.querySelector(".word");
+
+var textArea = document.querySelector("textarea");
+
+textArea.addEventListener("change", () => {
+  char.textContent = textArea.value.length;
+  word.textContent = textArea.value.trim().split("").length;
+});
