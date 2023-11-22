376a598c05a94f6beaad7e751637f6170c0a8630
Lucky <luckykashyap3430@gmail.com>
2023-11-22 22:38:31 +0530

    gsap basics

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/js/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/js/script.js
@@ -2,4 +2,18 @@
 
 //  transform: translateX(200px);
 
-gsap.to(".box", { x: 200 });
+// gsap.to(".box", {
+//   x: 1000,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   duration: 2,
+//   delay: 1,
+// });
+
+gsap.from(".box", {
+  x: 1000,
+  rotate: 360,
+  backgroundColor: "blue",
+  duration: 2,
+  delay: 1,
+});
