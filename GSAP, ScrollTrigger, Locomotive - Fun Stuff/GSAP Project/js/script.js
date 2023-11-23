a6b330649db91934efd1d665238cd6629ce03518
Lucky <luckykashyap3430@gmail.com>
2023-11-23 13:32:34 +0530

    heading animate

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP Project/js/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP Project/js/script.js
@@ -1,7 +1,37 @@
 // nav animate
 
-gsap.from(".nav h3", {
+// stagger one by one
+
+// gsap.from(".nav h3", {
+//   y: -50,
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.5,
+//   stagger: 1,
+// });
+
+// gsap.from(".main h1", {
+//   y: -50,
+//   opacity: 0,
+//   duration: 2,
+//   delay: 2,
+//   stagger: 1,
+// });
+
+var timeline = gsap.timeline();
+
+timeline.from(".nav h3", {
   y: -50,
   opacity: 0,
+  duration: 1,
   delay: 0.5,
+  stagger: 1,
+});
+
+timeline.from(".main h1", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  delay: 0.2,
+  stagger: 1,
 });
