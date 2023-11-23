56247c7e00507a0e7da410888d8a73a642dd05f5
Lucky <luckykashyap3430@gmail.com>
2023-11-23 14:14:40 +0530

    GSAP project created

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP Project/js/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP Project/js/script.js
@@ -23,15 +23,40 @@ var timeline = gsap.timeline();
 timeline.from(".nav h3", {
   y: -50,
   opacity: 0,
-  duration: 1,
+  duration: 0.8,
   delay: 0.5,
-  stagger: 1,
+  stagger: 0.8,
+});
+
+timeline.from(".main img", {
+  x: 100,
+  rotate: 45,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.2,
+  stagger: 0.8,
 });
 
 timeline.from(".main h1", {
-  y: -50,
+  x: -500,
   opacity: 0,
-  duration: 1,
+  duration: 0.8,
   delay: 0.2,
-  stagger: 1,
+  stagger: 0.8,
+});
+
+timeline.from(".main footer h3", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.2,
+  stagger: 0.8,
+});
+
+timeline.from(".main footer p", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.2,
+  stagger: 0.8,
 });
