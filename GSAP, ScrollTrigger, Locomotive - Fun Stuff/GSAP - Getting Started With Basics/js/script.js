f998e564f2d298fd29c5e966cae8b3640925bbd4
Lucky <luckykashyap3430@gmail.com>
2023-11-23 12:16:44 +0530

    gsap timeline

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/js/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/js/script.js
@@ -10,10 +10,62 @@
 //   delay: 1,
 // });
 
-gsap.from(".box", {
-  x: 1000,
+// gsap.from(".box", {
+//   x: 1000,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   duration: 2,
+//   delay: 1,
+// });
+
+// GSAP Timeline
+
+var timeline = gsap.timeline();
+
+timeline.to(".box1", {
+  x: 1200,
   rotate: 360,
-  backgroundColor: "blue",
+  scale: 0.5,
   duration: 2,
-  delay: 1,
+  // delay: 1,
 });
+
+timeline.to(".box2", {
+  x: 1200,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2,
+  // delay: 1,
+});
+
+timeline.to(".box3", {
+  x: 1200,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2,
+  // delay: 1,
+});
+
+// gsap.to(".box1", {
+//   x: 1200,
+//   rotate: 360,
+//   scale: 0.5,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// gsap.to(".box2", {
+//   x: 1200,
+//   rotate: 360,
+//   scale: 0.5,
+//   duration: 2,
+//   delay: 2,
+// });
+
+// gsap.to(".box3", {
+//   x: 1200,
+//   rotate: 360,
+//   scale: 0.5,
+//   duration: 2,
+//   delay: 3,
+// });
