723c49e9ebf82fe668958d7b002cb2cf3aa0a8df
Lucky <luckykashyap3430@gmail.com>
2023-11-23 16:41:56 +0530

    learning scrollTrigger

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Learning ScrollTrigger/js/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Learning ScrollTrigger/js/script.js
@@ -6,3 +6,27 @@ gsap.from(".page1 .circle", {
   duration: 2,
   rotate: 720,
 });
+
+gsap.from(".page2 .circle", {
+  scale: 0,
+  // delay: 1,
+  duration: 2,
+  rotate: 720,
+  // scrollTrigger: ".page2 .circle",
+  scrollTrigger: {
+    trigger: ".page2 .circle",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    // scrub: true,
+    scrub: 5,
+  },
+});
+
+// gsap.from(".page3 .circle", {
+//   scale: 0,
+//   delay: 1,
+//   duration: 2,
+//   rotate: 720,
+// });
