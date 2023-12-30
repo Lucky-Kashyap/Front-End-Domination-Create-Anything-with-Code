4227aad25b34d595b9af1de1a8681323d07097ee
Lucky <luckykashyap3430@gmail.com>
2023-12-30 17:59:40 +0530

    card created

--- React JS/React JS - Project 1/src/App.jsx
+++ React JS/React JS - Project 1/src/App.jsx
@@ -32,7 +32,18 @@ function App() {
         </ul>
       </nav>
 
-      <div className="w-[85%] h-full bg-red-100"></div>
+      <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+        <div className="card mr-3 mb-3 p-3 border shadow rounded h-[30vh] w-[18%]  flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3 w-full h-[80%] bg-no-repeat bg-contain bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+            }}
+          ></div>
+          <h1 className="hover:text-blue-300">Lorem ipsum dolor sit.</h1>
+        </div>
+      </div>
     </div>
   );
 }
