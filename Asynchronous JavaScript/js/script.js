b2abdf98ce2df027cdbb3c3ea95d83d20b1b69aa
Lucky <luckykashyap3430@gmail.com>
2024-01-30 22:11:43 +0530

    use clearInterval

--- Asynchronous JavaScript/js/script.js
+++ Asynchronous JavaScript/js/script.js
@@ -16,6 +16,16 @@
 //   console.log("Interval runs !!!");
 // }, 1000);
 
+let count = 0;
+
+let intervalID = setInterval(() => {
+  count++;
+  console.log(count);
+  if (count === 4) {
+    clearInterval(intervalID);
+  }
+}, 1000);
+
 // Fetch API - ye kisi aur url (API) par jaa kar kuchh data laayega ya data humaare paas se us url par lekar jaayega
 
 // Axios (or other HTTP libraries) - ye bhi wahi karega jo fetch karta hai bas ye thoda jda developer friendly hai
