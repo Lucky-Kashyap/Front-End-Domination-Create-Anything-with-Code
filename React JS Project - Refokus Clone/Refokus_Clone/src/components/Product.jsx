018f547c64f307368949a30f90b2bfd38fff5068
Lucky <luckykashyap3430@gmail.com>
2024-05-14 22:39:11 +0530

    apply moving vedio when mouse enter on product component

--- React JS Project - Refokus Clone/Refokus_Clone/src/components/Product.jsx
+++ React JS Project - Refokus Clone/Refokus_Clone/src/components/Product.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Button from "./Button";
 
-const Product = ({ val }) => {
+const Product = ({ val, mover, count }) => {
   return (
-    <div className="w-full py-20 text-white">
-      <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+    <div className="w-full py-20 h-[23rem] text-white">
+      <div
+        onMouseEnter={() => {
+          mover(count);
+        }}
+        className="max-w-screen-xl mx-auto flex justify-between items-center"
+      >
         <h1 className="text-6xl capitalize font-medium">{val.title}</h1>
         <div className="w-1/3">
           <p className="mb-10">{val.description}</p>
--- React JS Project - Refokus Clone/Refokus_Clone/src/components/Products.jsx
+++ React JS Project - Refokus Clone/Refokus_Clone/src/components/Products.jsx
@@ -1,5 +1,122 @@
-import React from "react";
+// import React, { useState } from "react";
+// import Product from "./Product";
+// import { motion } from "framer-motion";
+
+// const Products = () => {
+//   const products = [
+//     {
+//       title: "arqitel",
+//       description:
+//         "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",
+//       live: true,
+//       case: false,
+//     },
+//     {
+//       title: "cula",
+//       description:
+//         "We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",
+//       live: true,
+//       case: true,
+//     },
+//     {
+//       title: "layout land",
+//       description:
+//         "An interactive learning game that can educate and entertain you on the basics of web layouts in Webflow.",
+//       live: true,
+//       case: false,
+//     },
+//     {
+//       title: "TTR",
+//       description:
+//         "We've created an interactive site using generative AI to allow users to engage with our thinking about Ai, industry trends and design.",
+//       live: true,
+//       case: false,
+//     },
+//   ];
+
+//   const [pos, setPos] = useState(0);
+
+//   const mover = (val) => {
+//     setPos(val * 23);
+//   };
+
+//   return (
+//     <div className="mt-32 relative">
+//       {products.map((elem, index) => (
+//         <Product mover={mover} count={index} val={elem} key={index} />
+//       ))}
+//       <div className="absolute top-0 w-full h-full pointer-events-none">
+//         <motion.div
+//           initial={{ y: pos, x: "-50%" }}
+//           animate={{ y: pos + "rem" }}
+//           transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+//           className="absolute w-[32rem] h-[23rem] bg-white left-[44%] overflow-hidden"
+//         >
+//           <motion.div
+//             animate={{ y: -pos + "rem", opacity: 1 }}
+//             className="w-full h-full"
+//             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+//             initial={{ opacity: 0 }}
+//           >
+//             <video width="32rem" height="23rem" controls>
+//               <source
+//                 src="../assets/webflow-education-promo.mp4"
+//                 type="video/mp4"
+//               />
+//               Your browser does not support the video tag.
+//             </video>
+//           </motion.div>
+//           <motion.div
+//             animate={{ y: -pos + "rem" }}
+//             className="w-full h-full"
+//             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+//           >
+//             <video width="32rem" height="23rem" controls>
+//               <source
+//                 src="../assets/webflow-education-promo.mp4"
+//                 type="video/mp4"
+//               />
+//               Your browser does not support the video tag.
+//             </video>
+//           </motion.div>
+//           <motion.div
+//             animate={{ y: -pos + "rem" }}
+//             className="w-full h-full"
+//             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+//           >
+//             {" "}
+//             <video width="32rem" height="23rem" controls>
+//               <source
+//                 src="../assets/webflow-education-promo.mp4"
+//                 type="video/mp4"
+//               />
+//               Your browser does not support the video tag.
+//             </video>
+//           </motion.div>
+//           <motion.div
+//             animate={{ y: -pos + "rem" }}
+//             className="w-full h-full"
+//             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+//           >
+//             <video width="32rem" height="23rem" controls>
+//               <source
+//                 src="../assets/webflow-education-promo.mp4"
+//                 type="video/mp4"
+//               />
+//               Your browser does not support the video tag.
+//             </video>
+//           </motion.div>
+//         </motion.div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Products;
+
+import React, { useState } from "react";
 import Product from "./Product";
+import { motion } from "framer-motion";
 
 const Products = () => {
   const products = [
@@ -33,11 +150,43 @@ const Products = () => {
     },
   ];
 
+  const [pos, setPos] = useState(0);
+
+  const mover = (val) => {
+    setPos(val * 23);
+  };
+
   return (
-    <div className="mt-32">
+    <div className="mt-32 relative">
       {products.map((elem, index) => (
-        <Product val={elem} key={index} />
+        <Product mover={mover} count={index} val={elem} key={index} />
       ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none">
+        <motion.div
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + "rem" }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+          className="absolute w-[32rem] h-[23rem] bg-white left-[44%] overflow-hidden"
+        >
+          {products.map((_, index) => (
+            <motion.div
+              key={index}
+              animate={{ y: -pos + "rem", opacity: 1 }}
+              className="w-full h-full"
+              transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+              initial={{ opacity: 0 }}
+            >
+              <video width="100%" height="100%">
+                <source
+                  src="../assets/webflow-education-promo.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 };
